4071a88c6ceb7663724212c96afc1bbd7246331d
OSE-ICE <ose1975@gmail.com>
2023-10-03 14:01:55 -0400

    skilaverkefni 2

---
+++ example/skilaverkefni_2_1.dart
@@ -0,0 +1,13 @@
+
+
+void main(){
+
+  int sum = 0;
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+    sum += i;
+  }
+  print('Sum of the first 10 natural numbers is $sum.');
+}
+
+
---
+++ example/skilaverkefni_2_example.dart
@@ -0,0 +1,6 @@
+import 'package:skilaverkefni_2/skilaverkefni_2.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
